41929d9348774ad2907869319df398db421a10f2
Batouche <batouche@lm1-304-23976.local>
2022-06-10 17:02:11 +0300

    carousel experts done

--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -446,7 +446,8 @@
     /* --------------------------------------------------
     * industis carousel
     * --------------------------------------------------*/
-     $(".ot-industries-slider").owlCarousel({
+    $(window).on("load", function () {
+        $(".ot-industries-slider").owlCarousel({
         stagePadding: 365,
         items:2,
         nav:false,
@@ -454,24 +455,25 @@
         loop:true,
         navText: ['<i class="flaticon-back"></i>', '<i class="flaticon-right-arrow-1"></i>'],
         responsive:{
-            1600:{
-                stagePadding: 365,
-                items:2
-            },
-            1200:{
-                stagePadding: 200,
-                items:2
-            },
-            992:{
-                stagePadding: false,
-                items:2
-            },
-            0:{
-                stagePadding: false,
-                items:1
-            }
+        1600:{
+            stagePadding: 365,
+            items:2
+        },
+        1200:{
+            stagePadding: 200,
+            items:2
+        },
+        992:{
+            stagePadding: false,
+            items:2
+        },
+        0:{
+            stagePadding: false,
+            items:1
         }
-     });
+        }
+        });
+    });
      
     $(window).on("load", function(){
         $('.projects-grid').each( function(){
@@ -503,5 +505,43 @@
         });
     });
 
+    /* --------------------------------------------------
+    * Experts carousel
+    * --------------------------------------------------*/
+    $(window).on("load", function () {
+        $(".owl-carousel").owlCarousel({
+            nav:true,
+            items: 4,
+        });
+
+        $(".ot-experts-slider").owlCarousel({
+        // stagePadding: 65,
+        // items: 5,
+        // nav: true,
+        // dots: true,
+        // loop: false,
+        // rewind: true,
+        // autoWidth: true,
+        // loop:true,
+        margin:10,
+        nav:true,
+        navText: ['<i class="flaticon-back"></i>', '<i class="flaticon-right-arrow-1"></i>'],
+        responsive:{
+        1000:{
+            stagePadding: 65,
+            items:1
+        },
+        600:{
+            stagePadding: false,
+            items:1
+        },
+        0:{
+            stagePadding: false,
+            items:1
+        }
+        }
+        });
+    });
 
 })( jQuery );
+
--- src/components/home/Experts.js
+++ src/components/home/Experts.js
@@ -3,111 +3,140 @@ import React from 'react'
 const Experts = () => {
   return (
     <>
-        <section className="team-v5">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 text-center">
-                        <div className="ot-heading">
-                            <span>{"// Notre Equipe"}</span>
-                            <h2 className="main-heading">Meet Our Experts</h2>
-                        </div>
-                        <div className="space-5"></div>
-                        <p>Nous aidons les entreprises à augmenter leur rentabilité grâce
-                        à des services personnalisés,<br/>assurés par une équipe expérimentée, réactive et à l'écoute.</p>
-                        <div className="space-20"></div>
-                    </div>
-                </div>
-                <div className="row no-margin">
-                    {/* <!-- person one --> */}
-                    <div className="col-lg-3 col-md-6 no-padding">
-                        <div className="team-wrap v3">
-                            <div className="team-thumb">
-                                <img src={`${process.env.PUBLIC_URL}/static/images/team/user1.jpg`} alt="u1" />
-                                <div className="team-social flex-middle">
-                                    <div>
-                                        <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
-                                        <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
-                                        <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
-                                        <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="team-info">
-                                <h4>Amine Bennacef</h4>
-                                <span>Co-Founder of company</span>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- END person one --> */}
+    <section className="team-v5">
+        <div className="container">
 
-                    {/* <!-- person two --> */}
-                    <div className="col-lg-3 col-md-6 no-padding">
-                        <div className="team-wrap v3">
-                            <div className="team-thumb">
-                                <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
-                                <div className="team-social flex-middle">
-                                    <div>
-                                        <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
-                                        <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
-                                        <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
-                                        <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="team-info">
-                                <h4>Naim Midni</h4>
-                                <span>Finance Manager</span>
-                            </div>
-                        </div>
+            <div className="row">
+                <div className="col-lg-12 text-center">
+                    <div className="ot-heading">
+                        <span>{"// Notre Equipe"}</span>
+                        <h2 className="main-heading">Meet Our Experts</h2>
                     </div>
-                    {/* <!-- END person two --> */}
-
-                    {/* <!-- person three --> */}
-                    <div className="col-lg-3 col-md-6 no-padding">
-                        <div className="team-wrap v3">
-                            <div className="team-thumb">
-                                <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
-                                <div className="team-social flex-middle">
-                                    <div>
-                                        <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
-                                        <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
-                                        <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
-                                        <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="team-info">
-                                <h4>Billal Zidi</h4>
-                                <span>Sales Manager AT-AI</span>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- END person three --> */}
-
-                    {/* <!-- person four --> */}
-                    <div className="col-lg-3 col-md-6 no-padding">
-                        <div className="team-wrap v3">
-                            <div className="team-thumb">
-                                <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
-                                <div className="team-social flex-middle">
-                                    <div>
-                                        <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
-                                        <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
-                                        <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
-                                        <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="team-info">
-                                <h4>Salim Babaci</h4>
-                                <span>Sales Manager Geo</span>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- END person four --> */}
+                    <div className="space-5"></div>
+                    <p>Nous aidons les entreprises à augmenter leur rentabilité grâce
+                    à des services personnalisés,<br/>assurés par une équipe expérimentée, réactive et à l'écoute.</p>
+                    <div className="space-20"></div>
                 </div>
             </div>
-        </section>
+
+            {/* Start expert Row */}
+            <div className="row ">
+                <div className="owl-carousel owl-theme owl-loaded">
+                    <div className="owl-stage-outer">
+                        <div className="owl-stage">
+                            <div className="owl-item">
+                                <div className="">
+                                    <div className="team-wrap v3">
+                                        <div className="team-thumb">
+                                            <img src={`${process.env.PUBLIC_URL}/static/images/team/user1.jpg`} alt="u" />
+                                            <div className="team-social flex-middle">
+                                                <div>
+                                                    <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
+                                                    <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                                    <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
+                                                    <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="team-info">
+                                                <h4>Amine Bennacef</h4>
+                                                <span>Co-Founder of company</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                            
+                            <div className="owl-item">
+                                <div className="">
+                                    <div className="team-wrap v3">
+                                        <div className="team-thumb">
+                                            <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
+                                            <div className="team-social flex-middle">
+                                                <div>
+                                                    <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
+                                                    <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                                    <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
+                                                    <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="team-info">
+                                            <h4>Salim Babaci</h4>
+                                            <span>Sales Manager Geo</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="owl-item">
+                                <div className="">
+                                    <div className="team-wrap v3">
+                                        <div className="team-thumb">
+                                            <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
+                                            <div className="team-social flex-middle">
+                                                <div>
+                                                    <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
+                                                    <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                                    <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
+                                                    <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="team-info">
+                                            <h4>Billal Zidi</h4>
+                                            <span>Sales Manager AT-AI</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="owl-item">
+                                <div className="">
+                                    <div className="team-wrap v3">
+                                        <div className="team-thumb">
+                                            <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
+                                            <div className="team-social flex-middle">
+                                                <div>
+                                                    <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
+                                                    <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                                    <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
+                                                    <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="team-info">
+                                            <h4>Name here</h4>
+                                            <span>Position here</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="owl-item">
+                                <div className="">
+                                    <div className="team-wrap v3">
+                                        <div className="team-thumb">
+                                            <img src={`${process.env.PUBLIC_URL}/static/images/team/user.jpg`} alt="u" />
+                                            <div className="team-social flex-middle">
+                                                <div>
+                                                    <a rel="nofollow" href="twitter.com" className="twitter"><i className="fab fa-twitter"></i></a>
+                                                    <a rel="nofollow" href="linkedin.com" className="linkedin"><i className="fab fa-linkedin-in"></i></a>
+                                                    <a rel="nofollow" href="pinterest.com" className="pinterest"><i className="fab fa-pinterest-p"></i></a>
+                                                    <a rel="nofollow" href="instagram.com" className="instagram"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="team-info">
+                                            <h4>Name here</h4>
+                                            <span>Position here</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {/* End expert Row */}
+        </div>
+    </section>
     </>
   )
 }
