f24630587319843ab4e85ac16d83ca02615bfdc0
OussamaBATOUCHE <ossama12batouche@gmail.com>
2022-09-04 17:18:29 +0300

    Updates

---
+++ precache-manifest.bf9e8934a9466383c91b2923f62c3769.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bebf93ff1453e59f20ec",
+    "url": "/i2b-dz-react/static/js/main.bebf93ff.chunk.js"
+  },
+  {
+    "revision": "8af58df3cfe05ab13882",
+    "url": "/i2b-dz-react/static/js/1.8af58df3.chunk.js"
+  },
+  {
+    "revision": "0dfe028c1c9f6a60cd0d",
+    "url": "/i2b-dz-react/static/js/runtime~main.0dfe028c.js"
+  },
+  {
+    "revision": "56bbf42d4e7d6986331390e74ef8459e",
+    "url": "/i2b-dz-react/index.html"
+  }
+];
